24feb588ca44980f7e516868ace403a24745a2f9
Dsgathebest <gomezabrildaniel1234@gmail.com>
2023-10-04 15:36:03 -0500

    [Comment] The page displays gifs and maintains a history in local storage

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,24 @@
+import { Component, Output } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor(private gifService: GifsService){
+
+  }
+
+  get tags(){
+    return this.gifService.tagsHistory
+  }
+
+  searchTag(tag:string):void{
+    this.gifService.searchTag(tag);
+  }
+
+
+}
